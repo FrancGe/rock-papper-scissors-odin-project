3fcaba8b1566158515d960735684e26914fd7cb3
Franco Genoff <francogenoff@gmail.com>
2026-06-16 20:53:41 +0000

    Reformando el proyecto, agregando botones, interfaz grafica simple, contador de puntos y rondas

--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
-console.log("Conectado...");
+const piedra = document.querySelector(".piedra")
+const papel = document.querySelector(".papel")
+const tijera = document.querySelector(".tijera")
+const persona = document.querySelector("#persona");
+const pc = document.querySelector("#pc");
+const vs = document.querySelector("#vs");
+const marcador = document.querySelector("#marcador");
 
-// Numero random de el 1 al 100;
-let randomInt = Math.floor(Math.random() * 100);
+let puntosHumano = "";
+let puntosPC = "";
+let rondasJugadas = "";
 
 // Definiendo las elecciones de la PC
 function getRandomInt() {
-     if(randomInt >= 10 && randomInt < 50) {
-        return "Piedra";
-     } else if(randomInt >= 50 && randomInt < 75) {
-        return "Papel";
-     } else if(randomInt >= 75) {
-        return "Tijeras";
+    let randomInt = Math.floor(Math.random() * 100); // Numero random de el 1 al 100;
+     if(randomInt < 33) {
+        return "Piedra!";
+     } else if(randomInt < 66) {
+        return "Papel!";
+     } else if(randomInt > 66) {
+        return "Tijeras!";
      } else {
         return "";
      }
@@ -19,30 +27,167 @@ function getComputerChoice() {
     let respuestaPc = getRandomInt();
     return respuestaPc;
 }
-let eleccionPC = getComputerChoice();
-
-
-// Definiendo las elecciones de la persona
-function getHumanChoice() {
-    let respuesta = prompt("Pieda papel o tijeras!", "Piedra");
-    return respuesta;
-}
-let eleccionHumano = getHumanChoice();
-
 
 // Comparando ambas elecciones para definir el ganador
 function comparacion() {
     if(eleccionHumano === eleccionPC) {
         return "Empate";
-    } else if(eleccionHumano === "Piedra" && eleccionPC === "Tijeras") {
+    } else if(eleccionHumano === "Piedra!" && eleccionPC === "Tijeras!") {
         return "Punto para el humano!";
-    } else if(eleccionHumano === "Tijeras" && eleccionPC === "Papel") {
+    } else if(eleccionHumano === "Tijera!" && eleccionPC === "Papel!") {
         return "Punto para el humano!"
-    } else if(eleccionHumano === "Papel" && eleccionPC === "Piedra") {
+    } else if(eleccionHumano === "Papel!" && eleccionPC === "Piedra!") {
         return "Punto para el humano!";
     } else {
         return "Punto para la PC!";
     }
+    let resultado = "";
 }
 
-console.log(comparacion());
+// Haciendo que la PC elija al mismo tiempo que la persona
+let eleccionPC = "";
+
+// Definiendo elecciones de la persona mediante los botones
+
+
+
+let eleccionHumano = "";
+    piedra.addEventListener("click", function () {
+        if(rondasJugadas < 5) {
+        persona.innerHTML = "";
+        pc.innerHTML = "";
+        vs.innerHTML = "";
+
+        eleccionHumano = "Piedra!";
+        parrafoHumano = document.createElement("p");
+        parrafoHumano.textContent = eleccionHumano;
+        persona.appendChild(parrafoHumano);
+
+        eleccionPC = getComputerChoice();
+        parrafoPC = document.createElement("p");
+        parrafoPC.textContent = eleccionPC;
+        pc.appendChild(parrafoPC);
+
+        resultado = comparacion();
+        parrafo = document.createElement("p");
+        parrafo.textContent = resultado;
+        vs.appendChild(parrafo);
+
+        resultadoRondas = comparacion();
+        if(resultadoRondas === "Punto para el humano!") {
+            puntosHumano++;
+        } else if(resultadoRondas === "Punto para la PC!") {
+            puntosPC++;
+        } 
+        rondasJugadas++;
+        marcador.textContent = `Humano: ${puntosHumano} | ${puntosPC} :PC`;
+
+            if(rondasJugadas === 5) {
+                const mensajeGanador = document.createElement("p");
+                if(puntosHumano > puntosPC) {
+                    mensajeGanador.textContent = "GANASTE!!";
+                } else if(puntosHumano == puntosPC) {
+                    mensajeGanador.textContent = "EMPATE!!";
+                } else {
+                    mensajeGanador.textContent = "PERDISTE!! Vuelve a intentarlo";
+                }
+
+                ganador.appendChild(mensajeGanador);
+            }
+        }
+    });
+
+    papel.addEventListener("click", function () {
+        if(rondasJugadas < 5) {
+        persona.innerHTML = "";
+        pc.innerHTML = "";
+        vs.innerHTML = "";
+
+        eleccionHumano = "Papel!"
+        parrafoHumano = document.createElement("p");
+        parrafoHumano.textContent = eleccionHumano;
+        persona.appendChild(parrafoHumano);
+
+        eleccionPC = getComputerChoice();
+        parrafoPC = document.createElement("p");
+        parrafoPC.textContent = eleccionPC;
+        pc.appendChild(parrafoPC);
+
+        resultado = comparacion();
+        parrafo = document.createElement("p");
+        parrafo.textContent = resultado;
+        vs.appendChild(parrafo);
+
+        resultadoRondas = comparacion();
+        if(resultadoRondas === "Punto para el humano!") {
+            puntosHumano++;
+        } else if(resultadoRondas === "Punto para la PC!") {
+            puntosPC++;
+        } 
+        rondasJugadas++;
+        marcador.textContent = `Humano: ${puntosHumano} | ${puntosPC} :PC`;
+
+            if(rondasJugadas === 5) {
+                const mensajeGanador = document.createElement("p");
+                if(puntosHumano > puntosPC) {
+                    mensajeGanador.textContent = "GANASTE!!";
+                } else if(puntosHumano == puntosPC) {
+                    mensajeGanador.textContent = "EMPATE!!";
+                } else {
+                    mensajeGanador.textContent = "PERDISTE!! Vuelve a intentarlo";
+                }
+
+                ganador.appendChild(mensajeGanador);
+            }
+        }
+    });
+
+    tijera.addEventListener("click", function () {
+        if(rondasJugadas < 5) {
+        persona.innerHTML = "";
+        pc.innerHTML = "";
+        vs.innerHTML = "";
+
+        eleccionHumano = "Tijera!";
+        parrafoHumano = document.createElement("p");
+        parrafoHumano.textContent = eleccionHumano;
+        persona.appendChild(parrafoHumano);
+
+        eleccionPC = getComputerChoice();
+        parrafoPC = document.createElement("p");
+        parrafoPC.textContent = eleccionPC;
+        pc.appendChild(parrafoPC);
+
+        resultado = comparacion();
+        parrafo = document.createElement("p");
+        parrafo.textContent = resultado;
+        vs.appendChild(parrafo);
+
+        resultadoRondas = comparacion();
+        if(resultadoRondas === "Punto para el humano!") {
+            puntosHumano++;
+        } else if(resultadoRondas === "Punto para la PC!") {
+            puntosPC++;
+        } 
+        rondasJugadas++;
+        marcador.textContent = `Humano: ${puntosHumano} | ${puntosPC} :PC`;
+
+        if(rondasJugadas === 5) {
+                const mensajeGanador = document.createElement("p");
+                if(puntosHumano > puntosPC) {
+                    mensajeGanador.textContent = "GANASTE!!";
+                } else if(puntosHumano == puntosPC) {
+                    mensajeGanador.textContent = "EMPATE!!";
+                } else {
+                    mensajeGanador.textContent = "PERDISTE!! Vuelve a intentarlo";
+                }
+
+                ganador.appendChild(mensajeGanador);
+            }
+    }
+    });
+
+
+
+
+
